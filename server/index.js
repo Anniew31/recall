903fc3bdb808c78bca4ce945e2cc6a576f827d98
Annie Weng <126124914+Anniew31@users.noreply.github.com>
2026-06-23 15:10:54 -0400

    implement websocket server with room creation and joining

---
+++ server/index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const { createServer } = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
+});
+
+const rooms = {}
+
+io.on('connection', (socket) => {
+    console.log('a user connected:', socket.id)
+
+    socket.on('create_room', (data) => {
+        const roomCode = Math.random().toString(36).substring(2, 7).toUpperCase()
+        rooms[roomCode] = { players: [] }
+        rooms[roomCode].players.push({ id: socket.id, name: data.playerName, isHost: true })
+        socket.join(roomCode)
+        socket.emit('room_created', { roomCode })
+        io.to(roomCode).emit('player_list_updated', rooms[roomCode].players)
+    })
+
+    socket.on('join_room', (data) => {
+        if (!(data.roomCode in rooms)) {
+            socket.emit('error', { message: 'Room not found' })
+            return
+        }
+        socket.join(data.roomCode)
+        rooms[data.roomCode].players.push({ id: socket.id, name: data.playerName, isHost: false })
+        io.to(data.roomCode).emit('player_list_updated', rooms[data.roomCode].players)
+    })
+
+    socket.on('disconnect', () => {
+        for (const roomCode in rooms) {
+            const updatedPlayers = rooms[roomCode].players.filter(p => p.id !== socket.id)
+            if (updatedPlayers.length !== rooms[roomCode].players.length) {
+                rooms[roomCode].players = updatedPlayers
+                io.to(roomCode).emit('player_list_updated', rooms[roomCode].players)
+            }
+        }
+    })
+})
+
+httpServer.listen(3001, () => {
+  console.log('server running on port 3001')
+})
