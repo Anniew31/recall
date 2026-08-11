a5bed02809c67f29821a65ba36bb863f46d065a3
Annie Weng <126124914+Anniew31@users.noreply.github.com>
2026-08-11 09:53:43 -0400

    fix: remove undefined var

--- client/src/components/Question-Setup.tsx
+++ client/src/components/Question-Setup.tsx
@@ -63,7 +63,6 @@ export default function QuestionSetup({questionCount, topic, notesText, playerNa
                 questionId
             })
         } catch (err) {
-            setIsSubmitting(false);
             setError("Something went wrong submitting your question. Please try again.")
         }
     }
